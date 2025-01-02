18d439da0ded88e30ad70d086544d1647c4a4e31
Juli4nHuber <juliangabrielhr@gmail.com>
2025-01-02 12:56:36 -0300

    fix asjkasjkdas

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,6 @@ import 'package:pokedex_flutter/pokemon_screen.dart';
 import 'package:pokedex_flutter/config/theme/app_theme.dart';
 import 'pokemon_provider.dart';
 
-
 void main() {
   runApp(const MainApp());
 }
@@ -16,27 +15,14 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
-        providers: [
-          ChangeNotifierProvider(create: (_) => PokemonProvider())
-        ],
+        providers: [ChangeNotifierProvider(create: (_) => PokemonProvider())],
         child: MaterialApp(
           debugShowCheckedModeBanner: false,
           theme: AppTheme().getTheme(),
-          home: Scaffold(
-            body: Container(
-              decoration: const BoxDecoration(
-                gradient: LinearGradient(
-                  colors: [
-                  Color(0xFF181C14),
-                  Color(0xFF3C3D37),
-                  Color(0xFF3C3D37)
-                  ]
-                )
-              ),
-              child: const PokemonScreen()
-            )
-          )
-        )
-    );
-  }  
+          initialRoute: '/',
+          routes: {
+            '/': (context) => const PokemonScreen(),
+          },
+        ));
+  }
 }
--- lib/pokemon_screen.dart
+++ lib/pokemon_screen.dart
@@ -7,68 +7,90 @@ import 'package:pokedex_flutter/pokemon_provider.dart';
 
 import 'package:google_fonts/google_fonts.dart';
 
-
 class PokemonScreen extends StatelessWidget {
-
   static const name = 'pantalla-pokemón';
 
   const PokemonScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-
     final pokemon = context.watch<PokemonProvider>().pokemon;
     if (pokemon == null) {
-      return Center(
-        child: Image.asset('assets/images/spinnerPokeball.gif')
-      );
+      return Scaffold(
+          body: Container(
+              decoration: const BoxDecoration(
+                  gradient: LinearGradient(colors: [
+                Color(0xFF181C14),
+                Color(0xFF3C3D37),
+                Color(0xFF3C3D37)
+              ])),
+              child: Center(
+                  child: Image.asset('assets/images/spinnerPokeball.gif'))));
     }
 
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Row(
-          children: [
-            ElevatedButton(
-              onPressed: () => context.read<PokemonProvider>().changePreviousPokemon(),
-              style: TextButton.styleFrom(
-                backgroundColor: Colors.transparent,
-                iconColor: Colors.white,
-                padding: const EdgeInsets.all(0),
-                minimumSize: const Size(0, 0)
-              ),
-              child: const Icon(
-                Icons.arrow_left_rounded),
-            ),
-            Expanded(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  FadeInImage.assetNetwork(
-                    height: 264,
-                    width: 264,
-                    placeholder: 'assets/images/spinnerPokeball.gif',
-                    image: pokemon.image, 
-                    fit: BoxFit.contain
-                  ),
-                  Text(pokemon.name.toString().capitalize(), style: GoogleFonts.pressStart2p(color: Colors.white, fontSize: 16))
-                ],
-              ),
-            ),
-            ElevatedButton(
-              onPressed:() => context.read<PokemonProvider>().changeNextPokemon(),
-              style: TextButton.styleFrom(
-                backgroundColor: Colors.transparent,
-                iconColor: Colors.white,
-                padding: const EdgeInsets.all(0),
-                minimumSize: const Size(0, 0)
-              ),
-              child: const Icon(Icons.arrow_right_rounded),
-            )
-          ],
-        ), 
-        SlidersStadistics(data: pokemon.stadistics),
-      ]
-    );
+    return Scaffold(
+        body: GestureDetector(
+          onPanUpdate: (details) {
+            // Swiping in right direction.
+            if (details.delta.dx > 0) context.read<PokemonProvider>().changePreviousPokemon();
+            // Swiping in left direction.
+            if (details.delta.dx < 0) context.read<PokemonProvider>().changeNextPokemon();
+          },
+          child: Container(
+              decoration: const BoxDecoration(
+                  gradient: LinearGradient(colors: [
+                Color(0xFF181C14),
+                Color(0xFF3C3D37),
+                Color(0xFF3C3D37)
+              ])),
+              child:
+                  Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+                Row(
+                  children: [
+                    ElevatedButton(
+                      onPressed: () =>
+                          context.read<PokemonProvider>().changePreviousPokemon(),
+                      style: TextButton.styleFrom(
+                          backgroundColor: Colors.transparent,
+                          iconColor: Colors.white,
+                          padding: const EdgeInsets.all(0),
+                          minimumSize: const Size(0, 0)),
+                      child: const Icon(Icons.arrow_left_rounded),
+                    ),
+                    Expanded(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          FadeInImage.assetNetwork(
+                              key: ValueKey(pokemon.id),
+                              height: 264,
+                              width: 264,
+                              placeholder: 'assets/images/spinnerPokeball.gif',
+                              image: pokemon.image,
+                              fit: BoxFit.contain),
+                          Text(pokemon.name.toString().capitalize(),
+                              style: GoogleFonts.pressStart2p(
+                                  color: Colors.white, fontSize: 16))
+                        ],
+                      ),
+                    ),
+                    ElevatedButton(
+                      onPressed: () =>
+                          context.read<PokemonProvider>().changeNextPokemon(),
+                      style: TextButton.styleFrom(
+                          backgroundColor: Colors.transparent,
+                          iconColor: Colors.white,
+                          padding: const EdgeInsets.all(0),
+                          minimumSize: const Size(0, 0)),
+                      child: const Icon(Icons.arrow_right_rounded),
+                    )
+                  ],
+                ),
+                SlidersStadistics(data: pokemon.stadistics),
+                Text('Nº ${pokemon.id}',
+                              style: GoogleFonts.pressStart2p(
+                                  color: Colors.white, fontSize: 12))
+              ])),
+        ));
   }
 }
