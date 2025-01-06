78854dd71486551e08dad6e2d03946b8aa2a5b20
Juli4nHuber <juliangabrielhr@gmail.com>
2025-01-06 12:52:46 -0300

    Subo

--- lib/screens/index_pokemon/widgets/card_pokemon.dart
+++ lib/screens/index_pokemon/widgets/card_pokemon.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+
+import 'package:provider/provider.dart';
+
 import 'package:pokedex_flutter/repository/pokemon_api.dart';
 import 'package:pokedex_flutter/models/pokemon.dart';
+import 'package:pokedex_flutter/providers/pokemon_provider.dart';
 
 class CardPokemon extends StatefulWidget {
   final String idPokemon;
@@ -32,46 +36,52 @@ class _CardPokemonState extends State<CardPokemon> {
   @override
   Widget build(BuildContext context){
 
-    return Container(
-      decoration: BoxDecoration(
-        image: const DecorationImage(image: AssetImage('assets/images/grassCardBackground.jpg'), fit: BoxFit.cover, opacity: 0.5),
-        border: Border.all(width: 2, color: Colors.white),
-        borderRadius: const BorderRadius.all(Radius.circular(8))
-      ),
-      child: (pokemon != null) ? Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          AspectRatio(
-              aspectRatio: 1,
-              child: FadeInImage.assetNetwork(
-                placeholder: 'assets/images/spinnerPokeball.gif', 
-                image: pokemon!.sprites!.other!.showdown!.frontDefault!
+    return GestureDetector(
+      onDoubleTap: (){
+        context.read<PokemonProvider>().changePokemon(pokemon: (widget.idPokemon).toString());
+        Navigator.pushNamed(context, '/');
+      },
+      child: Container(
+        decoration: BoxDecoration(
+          image: const DecorationImage(image: AssetImage('assets/images/grassCardBackground.jpg'), fit: BoxFit.cover, opacity: 0.5),
+          border: Border.all(width: 2, color: Colors.white),
+          borderRadius: const BorderRadius.all(Radius.circular(8))
+        ),
+        child: (pokemon != null) ? Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            AspectRatio(
+                aspectRatio: 1,
+                child: FadeInImage.assetNetwork(
+                  placeholder: 'assets/images/spinnerPokeball.gif', 
+                  image: pokemon!.sprites!.other!.showdown!.frontDefault!
+                )
               )
+            ,
+            (pokemon!.types!.length == 1) ?
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Image.asset('assets/images/tipos/tipo_${pokemon!.types![0].type!.name}.png')
+                ],
+              ),
             )
-          ,
-          (pokemon!.types!.length == 1) ?
-          Expanded(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Image.asset('assets/images/tipos/tipo_${pokemon!.types![0].type!.name}.png')
-              ],
+            :
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Image.asset('assets/images/tipos/tipo_${pokemon!.types![0].type!.name}.png'),
+                  Image.asset('assets/images/tipos/tipo_${pokemon!.types![1].type!.name}.png')
+                ],
+              ),
             ),
-          )
-          :
-          Expanded(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Image.asset('assets/images/tipos/tipo_${pokemon!.types![0].type!.name}.png'),
-                Image.asset('assets/images/tipos/tipo_${pokemon!.types![1].type!.name}.png')
-              ],
-            ),
-          ),
-        ],
-      ) : null
+          ],
+        ) : null
+      ),
     );
   }
 }
